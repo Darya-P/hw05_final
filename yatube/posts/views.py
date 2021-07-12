b2f05caadf460ec32db1c740962864594c54cd49
Darya Ponomareva <darya-ponomareva08@yandex.ru>
2021-07-12 12:03:12 +0300

    Поле второго ревью

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -138,5 +138,7 @@ def profile_unfollow(request, username):
     author = get_object_or_404(User, username=username)
     if author != request.user:
         unfollow = Follow.objects.filter(author=author, user=request.user)
-        unfollow.delete()
+        unfollow.delete() 
         return redirect('profile', author.username)
+    else:
+        return redirect('index')
